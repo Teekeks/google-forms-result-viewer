4ecd8ba80222a3059d88a3eefd3e882b6f7865fa
Teekeks <info@teawork.de>
2024-05-09 10:52:56 +0200

    feat: added api call caching, set html title

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import logging
+
 from googleapiclient.discovery import build
 from google.oauth2 import service_account
 from aiohttp_jinja2 import setup as jinja2_setup, template
@@ -5,6 +7,7 @@ import jinja2
 import aiohttp
 from aiohttp import web
 from os import path
+from cachetools import cached, TTLCache
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
@@ -12,7 +15,7 @@ creds = service_account.Credentials.from_service_account_file('forms-viewer-825e
 
 service = build('sheets', 'v4', credentials=creds)
 sheet = service.spreadsheets()
-
+logging.basicConfig(level=logging.INFO)
 
 app: web.Application = web.Application()
 jinja2_setup(app,
@@ -21,15 +24,23 @@ jinja2_setup(app,
 routes = web.RouteTableDef()
 
 
+# cache the response for up to a minute
+@cached(cache=TTLCache(maxsize=1024, ttl=60))
+def get_sheet_values(sheet_id: str):
+    logging.info(f'fetching values from sheet {sheet_id}...')
+    sheet_data = sheet.get(spreadsheetId=sheet_id).execute()
+    title = sheet_data['properties']['title']
+    result = sheet.values().get(spreadsheetId=sheet_id, range='A1:AA').execute()
+    values = result.get('values', [])
+    questions = values[0]
+    answers = values[1:]
+    return list(enumerate([set(zip(questions, answer)) for answer in answers], start=1)), title
+
 @routes.get('/a/{sheet_id}')
 @template('result.html')
 async def show_results(request: web.Request):
-    result = sheet.values().get(spreadsheetId=request.match_info['sheet_id'], range='A1:AA').execute()
-    values = result.get('values', [])
-
-    questions = values[0]
-    answers = values[1:]
-    return {'answers': enumerate([zip(questions, answer) for answer in answers])}
+    data, title = get_sheet_values(request.match_info['sheet_id'])
+    return {'answers': data, 'title': title}
 
 
 app.add_routes(routes)
